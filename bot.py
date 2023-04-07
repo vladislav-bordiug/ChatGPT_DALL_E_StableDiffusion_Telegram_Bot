05630cbffa14e523f1480ec84e9e4793b01a0c14
Vladislav Bordiug <60838512+vladislav-bordiug@users.noreply.github.com>
2023-04-08 00:31:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import os
 
 from copilot import Copilot
 from text_to_image import TextToImage
+from text_to_img import TextToImg
 from dotenv import load_dotenv
 
 from telegram import (
@@ -24,7 +25,7 @@ from telegram.ext import (
 
 (ENTRY_STATE, 
 QUESTION_STATE,
-IMAGE_STATE, ) = range(3)
+IMAGE_STATE, DALL_E_STATE) = range(4)
 
 def _generate_copilot(prompt: str):
     """Gets answer from copilot"""
@@ -45,6 +46,14 @@ def _to_image(text: str):
     """Converts text to image"""
     
     tti = TextToImage()
+    i = tti.to_image(text)
+
+    return i
+  
+def _dall_e(text: str):
+    """Converts text to image"""
+    
+    tti = TextToImg()
     i = tti.to_image(text)
 
     return i
@@ -95,6 +104,21 @@ async def pre_image_handler(update: Update, context: ContextTypes):
     )
 
     return IMAGE_STATE
+  
+async def pre_dall_e(update: Update, context: ContextTypes):
+    """Ask the user for a query."""
+
+    button = [[KeyboardButton(text="Back")]]
+    reply_markup = ReplyKeyboardMarkup(
+        button, resize_keyboard=True
+    )
+
+    await update.message.reply_text(
+        "Enter your text: 👇🏻",
+        reply_markup=reply_markup,
+    )
+
+    return DALL_E_STATE
   
 #Handling the answer
 async def pre_query_answer_handler(update: Update, context: ContextTypes):
@@ -151,18 +175,47 @@ async def pre_image_answer_handler(update: Update, context: ContextTypes):
 
     return IMAGE_STATE
   
+ async def pre_dall_e_answer_handler(update: Update, context: ContextTypes):
+    """Display the answer to the user."""
+
+    button = [[KeyboardButton(text="Back")]]
+    reply_markup = ReplyKeyboardMarkup(
+        button, resize_keyboard=True
+    )
+
+    question = update.message.text
+
+    answer = _dall_e(question)
+
+    context.user_data['answer'] = answer
+    
+    if answer != None:
+      await update.message.reply_photo(
+            photo=answer, 
+            reply_markup=reply_markup, 
+            caption=question, 
+            )
+    else:
+      await update.message.reply_text(
+        "Your request activated the API's safety filters and could not be processed. Please modify the prompt and try again.", 
+        reply_markup=reply_markup,
+        )
+
+    return DALL_E_STATE
+  
 if __name__ == '__main__':
     load_dotenv()
 
     application = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).read_timeout(100).get_updates_read_timeout(100).build()
 
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start),MessageHandler(filters.Regex('^Question-Answering — ChatGPT 3.5 Turbo$'), pre_query_handler),MessageHandler(filters.Regex('^Image generation — Stable Diffusion$'), pre_image_handler),MessageHandler(filters.Regex('^Back$'), start)],
+        entry_points=[CommandHandler('start', start),MessageHandler(filters.Regex('^Question-Answering — ChatGPT 3.5 Turbo$'), pre_query_handler),MessageHandler(filters.Regex('^Image generation — DALL·E$'), pre_dall_e),MessageHandler(filters.Regex('^Image generation — Stable Diffusion$'), pre_image_handler),MessageHandler(filters.Regex('^Back$'), start)],
         states={
             ENTRY_STATE: [
                 CommandHandler('start', start),
                 MessageHandler(filters.Regex('^Back$'), start),
                 MessageHandler(filters.Regex('^Question-Answering — ChatGPT 3.5 Turbo$'), pre_query_handler),
+                MessageHandler(filters.Regex('^Image generation — DALL·E$'), pre_dall_e),
                 MessageHandler(filters.Regex('^Image generation — Stable Diffusion$'), pre_image_handler),
             ],
             QUESTION_STATE: [
@@ -174,6 +227,11 @@ if __name__ == '__main__':
                 CommandHandler('start', start),
                 MessageHandler(filters.Regex('^Back$'), start),
                 MessageHandler(filters.TEXT, pre_image_answer_handler),
+            ],
+            DALL_E_STATE: [
+                CommandHandler('start', start),
+                MessageHandler(filters.Regex('^Back$'), start),
+                MessageHandler(filters.TEXT, pre_dall_e_answer_handler),
             ],
         },
         fallbacks=[],
