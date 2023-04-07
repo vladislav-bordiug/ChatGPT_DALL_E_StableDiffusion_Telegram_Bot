7a6ce1067ec22cb5dd5483f1bb6f966d0c8e594f
Vladislav Bordiug <60838512+vladislav-bordiug@users.noreply.github.com>
2023-04-07 21:26:30 +0300

    Create copilot.py

---
+++ copilot.py
@@ -0,0 +1,43 @@
+import pip
+import openai
+import python-dotenv
+
+import os
+import json
+from dotenv import load_dotenv
+import openai
+
+class Copilot:
+
+    def clear_text(self, text):
+        a = text.replace("\n", " ")
+        b = a.split()
+        c = " ".join(b)
+
+        return c
+
+    def get_answer(self, question):
+        prompt = question
+        
+        load_dotenv()
+
+        openai.api_key = os.getenv("CHAT_GPT3_API_KEY")
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=1500,
+            temperature=0.5,
+        )
+
+        json_object = response
+
+        # Convert the JSON object to a JSON string
+        json_string = json.dumps(json_object)
+
+        # Parse the JSON string using json.loads()
+        parsed_json = json.loads(json_string)
+
+        text = parsed_json['choices'][0]['text']
+        cleared_text = self.clear_text(text)
+        
+        return cleared_text
