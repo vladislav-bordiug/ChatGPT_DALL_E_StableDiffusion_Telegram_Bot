d4af571113c78fe3bd084255edce3a4b749bb0d4
Vladislav Bordiug <60838512+vladislav-bordiug@users.noreply.github.com>
2023-04-10 22:54:20 +0300

    Update copilot.py

--- copilot.py
+++ copilot.py
@@ -16,24 +16,27 @@ class Copilot:
         prompt = question
         
         load_dotenv()
-
-        openai.api_key = os.getenv("CHAT_GPT3_API_KEY")
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=1500,
-            temperature=0.5,
-        )
-
-        json_object = response
-
-        # Convert the JSON object to a JSON string
-        json_string = json.dumps(json_object)
-
-        # Parse the JSON string using json.loads()
-        parsed_json = json.loads(json_string)
-
-        text = parsed_json['choices'][0]['text']
-        cleared_text = self.clear_text(text)
         
-        return cleared_text
+        openai.api_key = os.getenv("CHAT_GPT3_API_KEY")
+        try:
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                max_tokens=1500,
+                temperature=0.5,
+            )
+
+            json_object = response
+
+            # Convert the JSON object to a JSON string
+            json_string = json.dumps(json_object)
+
+            # Parse the JSON string using json.loads()
+            parsed_json = json.loads(json_string)
+
+            text = parsed_json['choices'][0]['text']
+            cleared_text = self.clear_text(text)
+
+            return cleared_text
+        except:
+            return
