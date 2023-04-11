e6a72040ad7f5cb5f3361ce76a0962cc750efc37
Vladislav Bordiug <60838512+vladislav-bordiug@users.noreply.github.com>
2023-04-11 23:08:06 +0300

    Update cryptopay.py

--- cryptopay.py
+++ cryptopay.py
@@ -6,9 +6,8 @@ load_dotenv()
 class Payment:
     async def payment(self, currency, cost):
         crypto = AioCryptoPay(token=os.getenv("CRYPTOPAY_KEY"), network=Networks.MAIN_NET)
-        
         invoice = await crypto.create_invoice(asset=currency, amount=cost)
-            return invoice
+        return invoice
 
     async def check_payment(self,invoice):
         invoices = await crypto.get_invoices(invoice_ids=invoice.invoice_id)
