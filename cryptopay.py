2fb7e4ac60610388882f78e3678ff9694beff768
Vladislav Bordiug <60838512+vladislav-bordiug@users.noreply.github.com>
2023-04-11 22:31:41 +0300

    Update cryptopay.py

--- cryptopay.py
+++ cryptopay.py
@@ -1,3 +1,9 @@
 from aiocryptopay import AioCryptoPay, Networks
 load_dotenv()
 crypto = AioCryptoPay(token=os.getenv("CRYPTOPAY_KEY"), network=Networks.MAIN_NET)
+
+invoice = await crypto.create_invoice(asset='TON', amount=1.5)
+print(invoice.pay_url)
+
+invoices = await crypto.get_invoices(invoice_ids=invoice.invoice_id)
+print(invoices.status)
