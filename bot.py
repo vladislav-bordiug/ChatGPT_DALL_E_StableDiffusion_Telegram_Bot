e0fc93aa1d088d6e2abf9ceb26d2ee509e32f1a8
Vladislav Bordiug <60838512+vladislav-bordiug@users.noreply.github.com>
2023-04-13 00:47:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,9 +67,9 @@ def _dall_e(text: str):
   
 async def start(update: Update, context: ContextTypes):
     """Start the conversation and ask user for an option."""
-    user_id = str(update.message.from_user.id)
+    user_id = update.message.from_user.id
     username = update.message.from_user.username
-    db_object.execute(f"SELECT user_id FROM users WHERE user_id = {user_id}")
+    db_object.execute(f"SELECT user_id FROM users WHERE user_id = {str(user_id)}")
     result = db_object.fetchone()
         
     button = [[KeyboardButton(text="Question-Answering — ChatGPT 3.5 Turbo")], [KeyboardButton(text="Image generation — DALL·E")], [KeyboardButton(text="Image generation — Stable Diffusion")],[KeyboardButton(text="My account | Buy")]]
