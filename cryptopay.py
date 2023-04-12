195c331f5890c7b9619de0b90ed9fc260aa186c0
Vladislav Bordiug <60838512+vladislav-bordiug@users.noreply.github.com>
2023-04-12 11:48:38 +0300

    Update cryptopay.py

--- cryptopay.py
+++ cryptopay.py
@@ -1,14 +1,27 @@
 import os
+from aiohttp import web
+
 from aiocryptopay import AioCryptoPay, Networks
-from dotenv import load_dotenv
+from aiocryptopay.models.update import Update
 load_dotenv()
 
-class Payment:
-    async def payment(self, currency, cost):
-        crypto = AioCryptoPay(token=os.getenv("CRYPTOPAY_KEY"), network=Networks.MAIN_NET)
-        invoice = await crypto.create_invoice(asset=currency, amount=cost)
-        return invoice
+web_app = web.Application()
+crypto = AioCryptoPay(token=os.getenv("CRYPTOPAY_KEY"), network=Networks.MAIN_NET)
 
-    async def check_payment(self,invoice):
-        invoices = await crypto.get_invoices(invoice_ids=invoice.invoice_id)
-        return invoices.status
+
+@crypto.pay_handler()
+async def invoice_paid(update: Update) -> None:
+    return update
+
+async def create_invoice(app) -> None:
+    invoice = await crypto.create_invoice(asset='TON', amount=1.5)
+    print(invoice.pay_url)
+
+async def close_session(app) -> None:
+    await crypto.close()
+
+
+web_app.add_routes([web.post(os.getenv("SECRET_DOMAIN"), crypto.get_updates)])
+web_app.on_startup.append(create_invoice)
+web_app.on_shutdown.append(close_session)
+web.run_app(app=web_app, host='localhost', port=3001)
