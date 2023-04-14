954ac8f230f33be66d06e6cceac6f0afc54398da
Vladislav Bordiug <vladislav.bordioug@gmail.com>
2023-04-14 18:52:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -469,6 +469,7 @@ if __name__ == '__main__':
     db_object = db_connection.cursor()
     application = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).read_timeout(100).get_updates_read_timeout(100).build()
     crypto = AioCryptoPay(token=os.getenv("CRYPTOPAY_KEY"), network=Networks.MAIN_NET)
+    print(crypto.get_rate("USDT","BTC"))
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start),MessageHandler(filters.Regex('^Back$'), start)],
         states={
