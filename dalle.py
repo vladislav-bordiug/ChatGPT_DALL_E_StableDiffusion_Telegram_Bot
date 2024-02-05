bffd328c20c77d56e0f7a79475ba8fc99c859b55
Vladislav Bordiug <vladislav.bordioug@gmail.com>
2024-02-05 18:32:05 +0300

    fix

--- dalle.py
+++ dalle.py
@@ -9,14 +9,11 @@ class DallE:
         client = OpenAI(
             api_key=os.getenv("OPENAI_API_KEY"),
         )
-        try:
-            response = client.images.generate(
-              model="dall-e-3",
-              prompt=prompt,
-              size="1024x1024",
-              quality="standard",
-              n=1,
-            )
-            return response.data[0].url
-        except:
-            return
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        return response.data[0].url
