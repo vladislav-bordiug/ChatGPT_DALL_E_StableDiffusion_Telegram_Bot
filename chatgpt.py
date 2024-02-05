ac61041293f05623f8665d58d52621a9c65df928
Vladislav Bordiug <vladislav.bordioug@gmail.com>
2024-02-05 16:15:03 +0300

    fix

--- chatgpt.py
+++ chatgpt.py
@@ -13,11 +13,14 @@ class Chatgpt:
         client = OpenAI(
             api_key=os.getenv("OPENAI_API_KEY"),
         )
-        response = client.completions.create(
-            model="gpt-3.5-turbo-instruct",
-            prompt=prompt,
-            max_tokens=1500,
-            temperature=0.5,
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model="gpt-3.5-turbo",
         )
 
-        return response
+        return response['choices'][0]['message']['content']
