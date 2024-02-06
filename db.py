354864d736fe93b292d0e4fdb992fe9a80d9048a
Vladislav <vladislav.bordioug@gmail.com>
2024-02-07 00:06:05 +0300

    fix

--- db.py
+++ db.py
@@ -2,11 +2,10 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
-db_object = db_connection.cursor()
-
 def is_user(user_id):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"SELECT user_id FROM users WHERE user_id = '{user_id}'")
     result = db_object.fetchone()
     db_object.close()
@@ -14,6 +13,9 @@ def is_user(user_id):
     return result
 
 def insert_user(user_id, username):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute("INSERT INTO users(user_id, username, chatgpt, dall_e, stable_diffusion) VALUES (%s, %s, %s, %s, %s)",(user_id, username, 3000, 3, 3))
     db_connection.commit()
     db_object.close()
@@ -21,6 +23,9 @@ def insert_user(user_id, username):
     return
 
 def get_chatgpt(user_id):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"SELECT chatgpt FROM users WHERE user_id = '{user_id}'")
     result = int(db_object.fetchone()[0])
     db_object.close()
@@ -28,6 +33,9 @@ def get_chatgpt(user_id):
     return result
 
 def set_chatgpt(user_id, result):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"UPDATE users SET chatgpt = {result} WHERE user_id = '{user_id}'")
     db_connection.commit()
     db_object.close()
@@ -35,6 +43,9 @@ def set_chatgpt(user_id, result):
     return result
 
 def get_dalle(user_id):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"SELECT dall_e FROM users WHERE user_id = '{user_id}'")
     result = int(db_object.fetchone()[0])
     db_object.close()
@@ -42,6 +53,9 @@ def get_dalle(user_id):
     return result
 
 def set_dalle(user_id, result):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"UPDATE users SET dall_e = {result} WHERE user_id = '{user_id}'")
     db_connection.commit()
     db_object.close()
@@ -49,6 +63,9 @@ def set_dalle(user_id, result):
     return result
 
 def get_stable(user_id):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"SELECT stable_diffusion FROM users WHERE user_id = '{user_id}'")
     result = int(db_object.fetchone()[0])
     db_object.close()
@@ -56,6 +73,9 @@ def get_stable(user_id):
     return result
 
 def set_stable(user_id, result):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"UPDATE users SET stable_diffusion = {result} WHERE user_id = '{user_id}'")
     db_connection.commit()
     db_object.close()
@@ -63,6 +83,9 @@ def set_stable(user_id, result):
     return result
 
 def get_userinfo(user_id):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"SELECT * FROM users WHERE user_id = '{user_id}'")
     result = db_object.fetchone()
     db_object.close()
@@ -70,6 +93,9 @@ def get_userinfo(user_id):
     return result
 
 def new_order(invoice_id, user_id, product):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute("INSERT INTO orders(invoice_id, user_id, product) VALUES (%s, %s, %s)", (invoice_id, user_id, product))
     db_connection.commit()
     db_object.close()
@@ -77,6 +103,9 @@ def new_order(invoice_id, user_id, product):
     return
 
 def get_orderdata(invoice_id):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"SELECT user_id, product FROM orders WHERE invoice_id = {invoice_id}")
     result = db_object.fetchone()
     db_object.close()
@@ -84,6 +113,9 @@ def get_orderdata(invoice_id):
     return result
 
 def update_chatgpt(user_id, invoice_id):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"UPDATE users SET chatgpt = chatgpt + 100000 WHERE user_id = '{user_id}'")
     db_object.execute(f"DELETE FROM orders WHERE invoice_id = {invoice_id}")
     db_connection.commit()
@@ -92,6 +124,9 @@ def update_chatgpt(user_id, invoice_id):
     return
 
 def update_dalle(user_id, invoice_id):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"UPDATE users SET dall_e = dall_e + 100 WHERE user_id = '{user_id}'")
     db_object.execute(f"DELETE FROM orders WHERE invoice_id = {invoice_id}")
     db_connection.commit()
@@ -100,6 +135,9 @@ def update_dalle(user_id, invoice_id):
     return
 
 def update_stable(user_id, invoice_id):
+    load_dotenv()
+    db_connection = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
+    db_object = db_connection.cursor()
     db_object.execute(f"UPDATE users SET stable_diffusion = stable_diffusion + 100 WHERE user_id = '{user_id}'")
     db_object.execute(f"DELETE FROM orders WHERE invoice_id = {invoice_id}")
     db_connection.commit()
