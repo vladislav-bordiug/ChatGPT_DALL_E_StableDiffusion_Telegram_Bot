3fab1bfccfc7fe081a60de44dac2b1c5eead2e38
Vladislav <vladislav.bordioug@gmail.com>
2024-03-31 01:14:08 +0300

    fix

--- db.py
+++ db.py
@@ -7,121 +7,93 @@ load_dotenv()
 class DataBase:
 
     async def is_user(user_id: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        result = await cursor.execute(f"SELECT user_id FROM users WHERE user_id = '{user_id}'")
-        await cursor.close()
-        await conn.close()
-        return result
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                result = await cursor.execute(f"SELECT user_id FROM users WHERE user_id = '{user_id}'")
+                return result
 
     async def insert_user(user_id: int, username: str):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute("INSERT INTO users(user_id, username, chatgpt, dall_e, stable_diffusion) VALUES (%s, %s, %s, %s, %s)", (user_id,username,3000,3,3))
-        await conn.commit()
-        await cursor.close()
-        await conn.close()
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute("INSERT INTO users(user_id, username, chatgpt, dall_e, stable_diffusion) VALUES (%s, %s, %s, %s, %s)", (user_id,username,3000,3,3))
+                await conn.commit()
 
     async def get_chatgpt(user_id: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"SELECT chatgpt FROM users WHERE user_id = '{user_id}'")
-        result = int((await cursor.fetchone())[0])
-        await cursor.close()
-        await conn.close()
-        return result
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"SELECT chatgpt FROM users WHERE user_id = '{user_id}'")
+                result = int((await cursor.fetchone())[0])
+                return result
 
     async def set_chatgpt(user_id: int, result: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"UPDATE users SET chatgpt = {result} WHERE user_id = '{user_id}'")
-        await conn.commit()
-        await cursor.close()
-        await conn.close()
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"UPDATE users SET chatgpt = {result} WHERE user_id = '{user_id}'")
+                await conn.commit()
 
     async def get_dalle(user_id: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"SELECT dall_e FROM users WHERE user_id = '{user_id}'")
-        result = int((await cursor.fetchone())[0])
-        await cursor.close()
-        await conn.close()
-        return result
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"SELECT dall_e FROM users WHERE user_id = '{user_id}'")
+                result = int((await cursor.fetchone())[0])
+                return result
 
     async def set_dalle(user_id: int, result: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"UPDATE users SET dall_e = {result} WHERE user_id = '{user_id}'")
-        await conn.commit()
-        await cursor.close()
-        await conn.close()
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"UPDATE users SET dall_e = {result} WHERE user_id = '{user_id}'")
+                await conn.commit()
 
     async def get_stable(user_id: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"SELECT stable_diffusion FROM users WHERE user_id = '{user_id}'")
-        result = int((await cursor.fetchone())[0])
-        await cursor.close()
-        await conn.close()
-        return result
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"SELECT stable_diffusion FROM users WHERE user_id = '{user_id}'")
+                result = int((await cursor.fetchone())[0])
+                return result
 
     async def set_stable(user_id: int, result: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"UPDATE users SET stable_diffusion = {result} WHERE user_id = '{user_id}'")
-        await conn.commit()
-        await cursor.close()
-        await conn.close()
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"UPDATE users SET stable_diffusion = {result} WHERE user_id = '{user_id}'")
+                await conn.commit()
 
     async def get_userinfo(user_id: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"SELECT * FROM users WHERE user_id = '{user_id}'")
-        result = await cursor.fetchone()
-        await cursor.close()
-        await conn.close()
-        return result
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"SELECT * FROM users WHERE user_id = '{user_id}'")
+                result = await cursor.fetchone()
+                return result
 
     async def new_order(invoice_id: int, user_id: int, product: str):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute("INSERT INTO orders(invoice_id, user_id, product) VALUES (%s, %s, %s)", (invoice_id, user_id, product))
-        await conn.commit()
-        await cursor.close()
-        await conn.close()
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute("INSERT INTO orders(invoice_id, user_id, product) VALUES (%s, %s, %s)", (invoice_id, user_id, product))
+                await conn.commit()
 
     async def get_orderdata(invoice_id: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"SELECT user_id, product FROM orders WHERE invoice_id = {invoice_id}")
-        result = await cursor.fetchone()
-        await cursor.close()
-        await conn.close()
-        return result
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"SELECT user_id, product FROM orders WHERE invoice_id = {invoice_id}")
+                result = await cursor.fetchone()
+                return result
 
     async def update_chatgpt(user_id: int, invoice_id: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"UPDATE users SET chatgpt = chatgpt + 100000 WHERE user_id = '{user_id}'")
-        await cursor.execute(f"DELETE FROM orders WHERE invoice_id = {invoice_id}")
-        await conn.commit()
-        await cursor.close()
-        await conn.close()
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"UPDATE users SET chatgpt = chatgpt + 100000 WHERE user_id = '{user_id}'")
+                await cursor.execute(f"DELETE FROM orders WHERE invoice_id = {invoice_id}")
+                await conn.commit()
 
     async def update_dalle(user_id: int, invoice_id: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"UPDATE users SET dall_e = dall_e + 100 WHERE user_id = '{user_id}'")
-        await cursor.execute(f"DELETE FROM orders WHERE invoice_id = {invoice_id}")
-        await conn.commit()
-        await cursor.close()
-        await conn.close()
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"UPDATE users SET dall_e = dall_e + 100 WHERE user_id = '{user_id}'")
+                await cursor.execute(f"DELETE FROM orders WHERE invoice_id = {invoice_id}")
+                await conn.commit()
 
     async def update_stable(user_id: int, invoice_id: int):
-        conn = await asyncpg.connect(getenv("DATABASE_URL"))
-        cursor = await conn.cursor()
-        await cursor.execute(f"UPDATE users SET stable_diffusion = stable_diffusion + 100 WHERE user_id = '{user_id}'")
-        await cursor.execute(f"DELETE FROM orders WHERE invoice_id = {invoice_id}")
-        await conn.commit()
-        await cursor.close()
-        await conn.close()
+        async with asyncpg.connect(getenv("DATABASE_URL")) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"UPDATE users SET stable_diffusion = stable_diffusion + 100 WHERE user_id = '{user_id}'")
+                await cursor.execute(f"DELETE FROM orders WHERE invoice_id = {invoice_id}")
+                await conn.commit()
