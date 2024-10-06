fcd4bb110e655bfcebd5bb85b5bfdd892d13e111
Vladislav <vladislav.bordioug@gmail.com>
2024-10-06 21:27:38 +0300

    fix

--- bot.py
+++ bot.py
@@ -347,42 +347,35 @@ async def buy(message: types.Message, state: FSMContext):
         reply_markup=keyboard,
     )
 
-# Checks payment
-@dp.callback_query()
-async def keyboard_callback(callback_query: types.CallbackQuery):
-    query = callback_query
-    invoice_id = int(query.data)
-    result = await DataBase.get_orderdata(invoice_id)
-    if result:
-        status = await CryptoPay.get_status(invoice_id)
-        if status == "active":
-            await query.answer("⌚️We have not received your payment yet")
-        elif status == "paid":
-            if result[1] == 'chatgpt':
-                await DataBase.update_chatgpt(result[0], invoice_id)
-                await query.answer("✅Successful payment, tokens were added to your account")
-            elif result[1] == 'dall_e':
-                await DataBase.update_dalle(result[0], invoice_id)
-                await query.answer("✅Successful payment, image generations were added to your account")
-            elif result[1] == 'stable':
-                await DataBase.update_stable(result[0], invoice_id)
-                await query.answer("✅Successful payment, image generations were added to your account")
-        elif status == "expired":
-            await query.answer("❎Payment has expired, create a new payment")
-    else:
-        await query.answer("✅You have already received your purchase")
-
-@app.post(getenv("WEBHOOK_PATH"))
+@app.post(getenv("/" + getenv("TELEGRAM_BOT_TOKEN")))
 async def bot_webhook(request: Request):
-    url = str(request.url)
-    print(url)
     update = types.Update(**await request.json())
     await dp.feed_webhook_update(bot, update)
     return web.Response()
 
+# Checks payment
+@app.post(getenv("/" + getenv("CRYPTOPAY_KEY")))
+async def payments_webhook(request: Request):
+    data = await request.json()
+    update_type = data['update_type']
+    if update_type == "invoice_paid":
+        invoice = data['payload']
+        invoice_id = invoice.invoice_id
+        result = await DataBase.get_orderdata(invoice_id)
+        if result[1] == 'chatgpt':
+            await DataBase.update_chatgpt(result[0], invoice_id)
+            await bot.send_message(result[0], "✅You have received 100000 ChatGPT tokens!")
+        elif result[1] == 'dall_e':
+            await DataBase.update_dalle(result[0], invoice_id)
+            await bot.send_message(result[0], "✅You have received 50 DALL·E image generations!")
+        elif result[1] == 'stable':
+            await DataBase.update_stable(result[0], invoice_id)
+            await bot.send_message(result[0], "✅You have received 50 Stable Diffusion image generations!")
+    return "OK"
+
 async def on_startup() -> None:
     await DataBase.open_pool()
-    url_webhook = getenv("BASE_WEBHOOK_URL") + getenv("WEBHOOK_PATH")
+    url_webhook = getenv("BASE_WEBHOOK_URL") + getenv("TELEGRAM_BOT_TOKEN")
     await bot.set_webhook(
         url=url_webhook
     )
