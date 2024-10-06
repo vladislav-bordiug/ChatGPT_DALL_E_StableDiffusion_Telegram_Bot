b37de9f200318c508836e16d22b9227d53aea8ae
Vladislav <vladislav.bordioug@gmail.com>
2024-10-06 16:04:14 +0300

    fix

--- bot.py
+++ bot.py
@@ -388,4 +388,4 @@ if __name__ == '__main__':
     )
     webhook_requests_handler.register(app, path=getenv("WEBHOOK_PATH"))
     setup_application(app, dp, bot=bot)
-    web.run_app(app, host=getenv("WEB_SERVER_HOST"), port=getenv("WEB_SERVER_PORT"))
+    web.run_app(app, host=getenv("WEB_SERVER_HOST"), port=int(getenv("WEB_SERVER_PORT")))
