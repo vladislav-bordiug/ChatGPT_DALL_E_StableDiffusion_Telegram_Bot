a6da23a74c0ed6c80bebe57f6773ac4177dcf34e
Vladislav <vladislav.bordioug@gmail.com>
2024-10-07 11:56:38 +0300

    fix

--- bot.py
+++ bot.py
@@ -100,17 +100,14 @@ async def question_handler(message: types.Message, state: FSMContext):
 async def reduce_messages(messages: List[Tuple[int, str, str, int]]) -> Tuple[int, int]:
     question_tokens = 0
     i = len(messages) - 1
-
     while i >= 0:
         if question_tokens + messages[i][3] < 128000:
             question_tokens += messages[i][3]
         else:
             break
         i -= 1
-
     for j in range(i+1):
         await DataBase.delete_message(messages[i][0])
-
     return i+1, question_tokens
 
 # Answer Handling
