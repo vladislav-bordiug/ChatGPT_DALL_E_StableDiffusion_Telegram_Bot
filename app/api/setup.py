142a04757a75f23493261c5d4de420302267ac3e
Vladislav <vladislav.bordioug@gmail.com>
2024-12-13 13:02:10 +0300

    fix

--- app/api/setup.py
+++ app/api/setup.py
@@ -5,12 +5,6 @@ from aiogram import Bot, Dispatcher
 
 def register_routes(router: APIRouter, dp: Dispatcher, bot: Bot, telegram_token: str, cryptopay_token: str):
     async def telegram_webhook(request):
-        raw_body = await request.body()
-
-        body_text = raw_body.decode("utf-8")
-
-        print("Request body:", body_text)
-
         return await bot_webhook(request, dp, bot)
 
     async def cryptopay_webhook(request):
