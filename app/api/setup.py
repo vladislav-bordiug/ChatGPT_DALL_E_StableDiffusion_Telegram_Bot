50c96812e75fe1b47f1c7530fd21104c56c6e202
Vladislav <vladislav.bordioug@gmail.com>
2024-12-16 23:58:46 +0300

    fix

--- app/api/setup.py
+++ app/api/setup.py
@@ -7,5 +7,5 @@ from app.services.db import DataBase
 def register_routes(router: APIRouter, database: DataBase, dp: Dispatcher, bot: Bot, telegram_token: str, cryptopay_token: str):
     routes_class = Handlers(database, dp, bot)
 
-    router.add_route("/" + telegram_token, routes_class.bot_webhook, methods=["POST"])
-    router.add_route("/" + cryptopay_token, routes_class.payments_webhook, methods=["POST"])
+    router.add_api_route("/" + telegram_token, routes_class.bot_webhook, methods=["POST"])
+    router.add_api_route("/" + cryptopay_token, routes_class.payments_webhook, methods=["POST"])
